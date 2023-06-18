6e8738cb5aeb8d0fbf86adc2881333102b7d24ec
Pablo Barbosa Silva <126425360+PabloPBS@users.noreply.github.com>
2023-06-18 19:33:56 -0300

    Elementos apareçem conforme scroll

--- script/script.js
+++ script/script.js
@@ -2,6 +2,27 @@ let menuMobile = document.querySelector('#menu')
 let abrirMenu = document.querySelector('#abrir-menu')
 let fecharMenu = document.querySelector('#fechar-menu')
 let botoesMenu = document.getElementsByClassName('botao-menu')
+const elementos = document.querySelectorAll('[data-anima]')
+const animacaoClass = 'animacao'
+const JavaScriptOn = 'js'
+
+function animaScroll() {
+    const topoPaginaNaJanela = window.scrollY + ((window.innerHeight * 3) / 4); //3/4 da janela
+    elementos.forEach(function(elemento){
+        elemento.classList.add(JavaScriptOn)
+        if(topoPaginaNaJanela > elemento.offsetTop) {
+            elemento.classList.add(animacaoClass)
+        } else {
+            elemento.classList.remove(animacaoClass)
+        }
+    })
+}
+
+if(elementos.length) {
+    window.addEventListener('scroll', function() {
+        animaScroll();
+    })
+}
 
 abrirMenu.addEventListener('click', show)
 fecharMenu.addEventListener('click', hide)
